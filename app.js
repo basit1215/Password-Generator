9dd40b26cc1ecf06c6fb2fb79a8be114248ac91c
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-11-01 21:42:39 +0500

    updated

--- app.js
+++ app.js
@@ -10,31 +10,30 @@ const allChars = upperCase + lowerCase + num + char;
 function createPassword() {
     let password = "";
 
-    // Ensuring at least one character from each type
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += num[Math.floor(Math.random() * num.length)];
     password += char[Math.floor(Math.random() * char.length)];
 
-    // Fill the rest of the password to meet the required length
     while (password.length < length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
 
-    // Shuffle the password for randomness
     password = password.split('').sort(() => Math.random() - 0.5).join('');
 
-    // Set the generated password to the input field
     pass.value = password;
 }
 
 function copyPassword() {
-    pass.select(); // Select the password text
-    document.execCommand("copy"); // Copy to clipboard
+    if (!pass.value) {
+        createPassword(); 
+    }
+    pass.select(); 
+    document.execCommand("copy"); 
     Swal.fire({
         title: 'Password Copied!',
         text: 'Password copied to clipboard!',
         icon: 'success',
         confirmButtonText: 'Okay'
-    }); // Show confirmation message with SweetAlert
+    }); 
 }
